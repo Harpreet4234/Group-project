346b2411033ec4efd7a7f38bdb50cf880fb7a285
Harpreet4234 <117226942+Harpreet4234@users.noreply.github.com>
2022-11-07 18:31:42 -0600

    Add files via upload

---
+++ Module 4 Group.py
@@ -0,0 +1,32 @@
+print("Please carefully read the following option's below?")
+print("1. Fail CSS-225")
+print("2. Uninstall Pycharm")
+print("3. Pass CSS-225 with an A")
+print("4. Erase Technology and video games from existence")
+print("5. Add $100,000 dollars in Student Loans")
+print("6. Exit")
+
+answer = input("To select an answer choose one of the following numbers: (1/2/3/4/5/6): ")
+
+
+if answer == "3":
+   print("Correct Passing CSS-225 with an A is the right choice")
+   print("well done")
+
+elif answer == "1":
+   print("Try again Failing CSS-225 is wrong")
+
+
+elif answer == "2":
+   print("Try again Uninstalling Pycharm is wrong")
+
+elif answer == "4":
+    print("Try again Erasing Technology and video games from existence is wrong")
+
+elif answer == "5":
+    print("Try again Add $100,000 dollars in Student Loans is wrong")
+
+elif answer == "6":
+   print("Try again Exiting is wrong")
+
+restart = input("Close and rerun to try again!")
